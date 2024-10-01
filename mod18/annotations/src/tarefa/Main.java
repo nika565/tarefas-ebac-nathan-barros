960ddec09f3b14850227c3848e37febffdadfe71
Nathan Barros <nathansousa0809@gmail.com>
2024-10-01 14:40:37 -0300

    Tarefa módulo 18

---
+++ mod18/annotations/src/tarefa/Main.java
@@ -0,0 +1,22 @@
+package tarefa;
+
+import java.lang.annotation.Annotation;
+
+public class Main {
+    public static void main(String[] args) {
+        // Obtendo a classe Produto
+        Class<Produto> produtoClass = Produto.class;
+
+        // Verificando se a classe possui a annotation Tabela
+        if (produtoClass.isAnnotationPresent(Tabela.class)) {
+            // Obtendo a annotation
+            Tabela tabelaAnnotation = produtoClass.getAnnotation(Tabela.class);
+
+            // Imprimindo o nome da tabela
+            System.out.println("Nome da tabela: " + tabelaAnnotation.nome());
+        } else {
+            System.out.println("A classe Produto não possui a annotation Tabela.");
+        }
+    }
+}
+
