20106323d36f777980e3188993501fa1a0441fc3
Nathan Barros <nathansousa0809@gmail.com>
2024-11-05 21:47:10 -0300

    mod42

---
+++ mod42/clienteservice/src/test/java/com/nathan/clienteservice/ClienteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.clienteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
